e0e37183fe85ccddd089b2c953d5496b7002850c
sakunaka39 <nakamura.shotaro.e8@s.gifu-u.ac.jp>
2024-04-24 10:57:29 +0900

    Initial Commit

---
+++ jissen240424/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jissen240424
+//
+//  Created by 中村咲太郎 on 2024/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jissen240424/jissen240424App.swift
@@ -0,0 +1,17 @@
+//
+//  jissen240424App.swift
+//  jissen240424
+//
+//  Created by 中村咲太郎 on 2024/04/24.
+//
+
+import SwiftUI
+
+@main
+struct jissen240424App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
